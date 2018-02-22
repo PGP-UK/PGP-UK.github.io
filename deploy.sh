3e72b523db5b05186523de19d651f7beba649316
IsmailM <ismail.moghul@gmail.com>
2018-02-22 14:34:39 +0000

    temporarily hard code the json assets

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ set -e # exit with nonzero exit code if anything fails
 ### REQUIRES RUBY 2.5.0 or higher
 
 
-gem install typhoeus
+# gem install typhoeus
 
 # generate the json files
-ruby generate_json_files.rb www/data/json
-ruby csv_parser.rb www/data/json
-mv phenotype.csv www/data/phenotype.csv
+# ruby generate_json_files.rb www/data/json
+# ruby csv_parser.rb www/data/json
+# mv phenotype.csv www/data/phenotype.csv

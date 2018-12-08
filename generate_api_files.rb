6bdef438e003f2ecda03cd080bfc1e9fccae0246
Ismail Moghul <ismail.moghul@gmail.com>
2018-12-08 20:47:32 +0000

    include download_urls in api

--- generate_api_files.rb
+++ generate_api_files.rb
@@ -24,6 +24,15 @@ def write_all_participants
   File.open(outfile, 'w') { |io| io.puts JSON.pretty_generate(JSON_DATA) }
 end
 
+def write_all_participants_as_nd_json
+  outfile = API_DIR + 'all_participants'
+  File.open(outfile, 'w') do |io|
+    JSON_DATA.each do |hex_id, data|
+      io.puts({ hex_id => data }.to_json)
+    end
+  end
+end
+
 def write_subset_files(dir, key)
   dir = API_DIR + dir
   FileUtils.mkdir_p dir
@@ -44,21 +53,41 @@ def write_all_subset(fname, key)
   File.open(outfile, 'w') { |io| io.puts JSON.pretty_generate(subset) }
 end
 
+def write_all_subset_as_ndjson(fname, key)
+  outfile = API_DIR + fname
+  File.open(outfile, 'w') do |io|
+    JSON_DATA.each do |hex_id, d|
+      next unless d.key? key
+      io.puts({ hex_id => d[key] }.to_json)
+    end
+  end
+end
+
 # RUN ANALYSIS
 API_DIR = Pathname.new('www/api/v1/')
 json_dir = Pathname.new('www/data/json')
 
-json_content = IO.read(json_dir + 'data.json')
-JSON_DATA = JSON.parse(json_content, symbolize_names: true)
+data_content = IO.read(json_dir + 'data.json')
+data = JSON.parse(data_content, symbolize_names: true)
+
+download_data_content = IO.read(json_dir + 'data_file_links.json')
+download_data = JSON.parse(download_data_content, symbolize_names: true)
+
+JSON_DATA = {}
+data.each do |hex_id, d|
+  f_data = download_data[hex_id]
+  d[:download_urls] = f_data unless f_data.nil?
+  JSON_DATA[hex_id] = d
+end
 
 clean_state
 
 write_participants_files
-write_all_participants
+write_all_participants_as_nd_json
 
 # get all possible keys in JSON_DATA
 key_types = JSON_DATA.values.map(&:keys).flatten.uniq
 key_types.each do |key|
   write_subset_files(key.to_s, key)
-  write_all_subset("all_#{key}", key)
+  write_all_subset_as_ndjson("all_#{key}", key)
 end

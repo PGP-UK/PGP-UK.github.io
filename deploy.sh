244d45a935e810bacb904c5767b908a37c7bb2e4
Ismail Moghul <ismail.moghul@gmail.com>
2019-02-12 23:43:40 +0000

    update deploy logic for API

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,20 @@ set -ex # exit immediately with nonzero exit code if anything fails
 # Install dependencies
 bundle install
 
+LATEST_API_VERSION="v1.0"
+
 # generate the json files
 bundle exec ruby scripts/generate_json_files.rb
 bundle exec ruby scripts/generate_ftp_links.rb
-bundle exec ruby scripts/generate_api_files.rb www/api/v1/
+bundle exec ruby scripts/generate_api_files.rb www/api/${LATEST_API_VERSION}
+
+# make /api == the latest version of the API documentation
+cp www/api/index.html www/api/${LATEST_API_VERSION}
+cp www/api/openapi.json www/api/${LATEST_API_VERSION}
+cp www/api/openapi.yaml www/api/${LATEST_API_VERSION}
+
+# make old API link work
+cp -r www/api/v1.0  www/api/v1
 
 # Copy the original files
 cp data/table.json www/data/json/table.json

76936f99a2bfc2c921ccf2d06fd6cf52bcba20ab
IsmailM <ismail.moghul@gmail.com>
2019-09-19 13:57:45 +0100

    change PGP JS module name

--- www/static/js/data.js
+++ www/static/js/data.js
@@ -1,32 +1,32 @@
-var ES; // define global ES (acronym for EBI_Search)  object
-if (!ES) {
-  ES = {};
+var PGP; // define global PGP (acronym for Personal Genome Project UK)  object
+if (!PGP) {
+  PGP = {};
 }
 
 (function() {
-  // ES module
-  ES.API_VERSION = 'v1.1';
-  ES.init = function() {
+  // PGP module
+  PGP.API_VERSION = 'v1.1';
+  PGP.init = function() {
     $.getJSON('/data/json/table.json', function(json) {
-      ES.table_json = json;
-      var datatable = ES.initializeTable(json, 'data_table');
-      $.getJSON('/api/' + ES.API_VERSION + '/all_participants.json', function(data_json) {
-        ES.data_json = data_json;
-        ES.makeIconClickable(datatable, 'data_table', data_json, '.file_btn');
-        ES.makeIconClickable(datatable, 'data_table', data_json, '.report_btn');
-        ES.makeIconClickable(datatable, 'data_table', data_json, '.trait_btn');
+      PGP.table_json = json;
+      var datatable = PGP.initializeTable(json, 'data_table');
+      $.getJSON('/api/' + PGP.API_VERSION + '/all_participants.json', function(data_json) {
+        PGP.data_json = data_json;
+        PGP.makeIconClickable(datatable, 'data_table', data_json, '.file_btn');
+        PGP.makeIconClickable(datatable, 'data_table', data_json, '.report_btn');
+        PGP.makeIconClickable(datatable, 'data_table', data_json, '.trait_btn');
       });
     });
   };
 
-  ES.initializeTable = function(dataset, tableId) {
+  PGP.initializeTable = function(dataset, tableId) {
     var dt = $('#' + tableId).DataTable({
       data: dataset,
       responsive: true,
       iDisplayLength: 10,
       pagingType: 'full',
       order: [[0, 'asc']],
-      search: ES.checkForDefaultSearch(),
+      search: PGP.checkForDefaultSearch(),
       bAutoWidth: false,
       columnDefs: [
         {
@@ -52,7 +52,7 @@ if (!ES) {
     return dt;
   };
 
-  ES.checkForDefaultSearch = function() {
+  PGP.checkForDefaultSearch = function() {
     var urlParams = new URLSearchParams(window.location.search);
     if (urlParams.has('hex_id')) {
       var hex_id = urlParams.get('hex_id');
@@ -63,7 +63,7 @@ if (!ES) {
     }
   };
 
-  ES.makeIconClickable = function(datatable, tableId, dataset, type) {
+  PGP.makeIconClickable = function(datatable, tableId, dataset, type) {
     $('#' + tableId).on('click', '.close', function() {
       var tr = $(this).closest('tr').prev()[0];
       var row = datatable.row(tr);
@@ -74,11 +74,11 @@ if (!ES) {
       var $tr = $(this).closest('tr');
       var id = $tr.find('.pgp_hex_id').text();
       var row = datatable.row($tr);
-      ES.openChildRow(row, $tr, dataset, id, type);
+      PGP.openChildRow(row, $tr, dataset, id, type);
     });
   };
 
-  ES.openChildRow = function(row, $tr, dataset, id, type) {
+  PGP.openChildRow = function(row, $tr, dataset, id, type) {
     var child_html;
     if (
       row.child.isShown() &&
@@ -90,11 +90,11 @@ if (!ES) {
     } else {
       var data = dataset[id];
       if (type == '.report_btn') {
-        child_html = ES.openReportsChildRow(data, id, type);
+        child_html = PGP.openReportsChildRow(data, id, type);
       } else if (type == '.trait_btn') {
-        child_html = ES.openTraitsChildRow(data, id, type);
+        child_html = PGP.openTraitsChildRow(data, id, type);
       } else if (type == '.file_btn') {
-        child_html = ES.openFilesChildRow(data, id, type);
+        child_html = PGP.openFilesChildRow(data, id, type);
       }
       if (child_html != undefined) {
         row.child(child_html).show();
@@ -103,7 +103,7 @@ if (!ES) {
     }
   };
 
-  ES.openReportsChildRow = function(data, id, type) {
+  PGP.openReportsChildRow = function(data, id, type) {
     var reports_html = $('#child_template')
       .clone()
       .attr('id', type.replace('.', '') + id)
@@ -122,13 +122,13 @@ if (!ES) {
       $(reports_html)
         .find('#child_template_body')
         .append($('<p id="genome_reports">'), $('<p id="methylome_reports">'));
-      ES.addReports(data, reports_html, 'genome_report', '#genome_reports');
-      ES.addReports(data, reports_html, 'methylome_report', '#methylome_reports');
+      PGP.addReports(data, reports_html, 'genome_report', '#genome_reports');
+      PGP.addReports(data, reports_html, 'methylome_report', '#methylome_reports');
     }
     return reports_html;
   };
 
-  ES.addReports = function(data, child_html, type, wrapper_id) {
+  PGP.addReports = function(data, child_html, type, wrapper_id) {
     if (data[type] == undefined) return;
     for (var i = 0; i < data[type].length; i++) {
       $('<a download="">')
@@ -144,7 +144,7 @@ if (!ES) {
     }
   };
 
-  ES.openTraitsChildRow = function(dataset, id, type) {
+  PGP.openTraitsChildRow = function(dataset, id, type) {
     var data;
     var traits_html = $('#child_template')
       .clone()
@@ -158,11 +158,11 @@ if (!ES) {
     } else {
       data = dataset.phenotype[0];
     }
-    traits_html = ES.updateTraitsHtml(data, traits_html);
+    traits_html = PGP.updateTraitsHtml(data, traits_html);
     return traits_html;
   };
 
-  ES.updateTraitsHtml = function(data, traits_html) {
+  PGP.updateTraitsHtml = function(data, traits_html) {
     if (data == null) {
       $(traits_html)
         .find('#child_template_body')
@@ -187,7 +187,7 @@ if (!ES) {
     return traits_html;
   };
 
-  ES.openFilesChildRow = function(data, id, type) {
+  PGP.openFilesChildRow = function(data, id, type) {
     var file_html = $('#child_template')
       .clone()
       .attr('id', type.replace('.', '') + id)
@@ -195,16 +195,16 @@ if (!ES) {
     $(file_html)
       .find('#child_template_header')
       .text('PGP-UK Data');
-    file_html = ES.addSection(file_html, 'Genome Data', 'genome');
-    file_html = ES.addSection(file_html, 'Methylome Data', 'methylome');
-    file_html = ES.addSection(file_html, 'Transcriptome Data', 'transcriptome');
-    file_html = ES.addSection(file_html, 'User Uploaded Data', 'user_uploaded');
-    data = ES.add_section_info(data.download_url);
-    file_html = ES.addFileButtonHtml(data, file_html);
+    file_html = PGP.addSection(file_html, 'Genome Data', 'genome');
+    file_html = PGP.addSection(file_html, 'Methylome Data', 'methylome');
+    file_html = PGP.addSection(file_html, 'Transcriptome Data', 'transcriptome');
+    file_html = PGP.addSection(file_html, 'User Uploaded Data', 'user_uploaded');
+    data = PGP.add_section_info(data.download_url);
+    file_html = PGP.addFileButtonHtml(data, file_html);
     return file_html;
   };
 
-  ES.addSection = function(file_html, title, klass) {
+  PGP.addSection = function(file_html, title, klass) {
     $(file_html)
       .find('#child_template_body')
       .append(
@@ -221,7 +221,7 @@ if (!ES) {
     return file_html;
   };
 
-  ES.add_section_info = function(data) {
+  PGP.add_section_info = function(data) {
     if (data == undefined) return
     for (var i = 0; i < data.length; i++) {
       type = data[i].type;
@@ -238,7 +238,7 @@ if (!ES) {
     return data;
   };
 
-  ES.addFileButtonHtml = function(data, file_html) {
+  PGP.addFileButtonHtml = function(data, file_html) {
     if (data == undefined) return;
     for (var i = 0; i < data.length; i++) {
       if (data[i].section === undefined) continue;
@@ -265,4 +265,4 @@ if (!ES) {
     }
     return file_html;
   };
-})(); // End of ES module
+})(); // End of PGP module
